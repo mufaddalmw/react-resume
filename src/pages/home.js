e05530a83017fe114b38e691d45eb911d5fd3cc2
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2018-07-14 09:57:19 +0400

    First commit

---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import profilePic from './mufaddal.jpg'
+
+
+export default class Home extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <div className="column small-3 w3-margin-top">
+                    <img className="thumbnail" src={profilePic} alt="Mufaddal Motorwala"/>
+                </div>
+                <div className="column small-9 w3-margin-top">
+                    <h1>Mufaddal Motorwala</h1>
+                    <h3>Front-end Developer</h3>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
