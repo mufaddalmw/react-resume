bab4d74a8af52a08e14be15766d258312a1b576a
Mufaddal Motorwala <mufaddal.motorwala@rbbideas.com>
2018-07-18 09:35:00 +0400

    added new image, added swiper dependency

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,24 +8,48 @@ export default class Portfolio extends Component {
             <React.Fragment>
                 <div className="grid-container">
                     <h1>Portfolio</h1>
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <a href="http://rbbideas.com/" target="_blank" rel="noopener noreferrer">
+                                    <figure>
+                                        <img src={img_rbbi} alt="RBBideas" />
+                                        <figcaption>RBBideas</figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="http://adib.ae/" target="_blank" rel="noopener noreferrer">
+                                    <figure>
+                                        <img src={img_adib} alt="Abu Dhabi Islamic Bank" />
+                                        <figcaption>Abu Dhabi Islamic Bank</figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="https://www.clevelandclinicabudhabi.ae/en/pages/default.aspx" target="_blank" rel="noopener noreferrer">
+                                    <figure>
+                                        <img src={img_rbbi} alt="Cleveland Clinic Abu Dhabi" />
+                                        <figcaption>Cleveland Clinic Abu Dhabi</figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="swiper-slide">
+                                <a href="https://www.famousbirthdays.com/" target="_blank" rel="noopener noreferrer">
+                                    <figure>
+                                        <img src={img_adib} alt="Famous Birthdays" />
+                                        <figcaption>Famous Birthdays</figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="swiper-slide">Slide 3</div>
+                            <div class="swiper-slide">Slide 3</div>
+                        </div>
+                    </div>
                     <ul>
                         <li>
-                            <a href="http://rbbideas.com/" target="_blank" rel="noopener noreferrer">
-                                <figure>
-                                    <img src={img_rbbi} alt="RBBideas"/>
-                                    <figcaption>RBBideas</figcaption>
-                                </figure>
-                            </a>
+                            
                         </li>
-                        <li>
-                            <a href="http://adib.ae/" target="_blank" rel="noopener noreferrer">
-                                <figure>
-                                    <img src={img_adib} alt="Abu Dhabi Islamic Bank" />
-                                    <figcaption>Abu Dhabi Islamic Bank</figcaption>
-                                </figure>
-                            </a></li>
-                        <li>Cleveland Clinic Abu Dhabi</li>
-                        <li>Famous Birthdays</li>
                         <li>Dubai Holding</li>
                         <li>Global Manufacturing &amp; Industrialisation Summit</li>
                     </ul>
