1e49b1ffa393f811be7be3bcba68b606bf536e22
mufaddalmw <mufaddal.motorwala@gmail.com>
2018-08-04 13:05:43 +0400

    added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express")
+var app = express();
+var port = process.env.PORT || 5000;
+var path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './build')));
+
+app.get('*', function(request, response) {
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  response.sendFile(filePath);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
