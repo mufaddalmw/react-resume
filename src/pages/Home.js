3876555c547bb0a7c9936fa47ddae9d0550fa53d
mufaddalmw <mufaddal.motorwala@gmail.com>
2018-08-11 14:46:12 +0400

    added procfile, edited skills, added active menu css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,15 +13,13 @@ export default class Home extends Component {
             <React.Fragment>
                 <Header isHomePage={true}/>
                 <div className="grid-container">
-                    <div className="grid-x">
-                        <div className="cell text-center">
+                    <div className="grid-y grid-padding-y text-center" style={{height: '500px'}}>
+                        <div className="cell small-8 medium-8 large-10">
                             <img className="thumbnail" src={profilePic} srcSet={`${profilePic2x} 2x`} alt="Mufaddal Motorwala"/>
                         </div>
-                        <div className="cell text-center">
+                        <div className="cell small-4 medium-4 large-2">
                             <h1>Mufaddal Motorwala</h1>
-                            <h3>Front-End Developer</h3>
-                            
-                                
+                            <p>Front-End Developer</p>
                         </div>
                     </div>
                 </div>
